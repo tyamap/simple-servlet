8a4b59006f04520ee8c22168c65847a33338962e
tyamap <tom333my@outlook.com>
2020-09-05 17:02:44 +0900

    add JPAでエンティティ管理

--- src/main/java/com/tyamap/simple_servlet/EventManager.java
+++ src/main/java/com/tyamap/simple_servlet/EventManager.java
@@ -3,67 +3,44 @@ package com.tyamap.simple_servlet;
 import java.util.Date;
 import java.util.List;
 
-import org.hibernate.Session;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import com.tyamap.simple_servlet.domain.Employee;
 import com.tyamap.simple_servlet.domain.Event;
-import com.tyamap.simple_servlet.util.HibernateUtil;
 
 public class EventManager {
+	private static EntityManagerFactory entityManagerFactory;
 
     public static void main(String[] args) {
-        EventManager mgr = new EventManager();
+        entityManagerFactory = Persistence.createEntityManagerFactory( "com.tyamap.simple_servlet.jpa" );
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
 
         // mvn exec:java -Dexec.mainClass="com.tyamap.simple_servlet.EventManager" -Dexec.args="store"
         if (args[0].equals("store")) {
-            mgr.createAndStoreEvent("My Event", new Date());
+            // entityManager.persist( new Event( "Our very first event!", new Date() ) );
+            // entityManager.persist( new Event( "A follow up event", new Date() ) );
         }
         // mvn exec:java -Dexec.mainClass="com.tyamap.simple_servlet.EventManager" -Dexec.args="list"
         else if (args[0].equals("list")) {
-            List events = mgr.listEvents();
-            for (int i = 0; i < events.size(); i++) {
-                Event theEvent = (Event) events.get(i);
-                System.out.println("Event: " + theEvent.getTitle() + " Time: " + theEvent.getDate());
+            List<Event> result = entityManager.createQuery( "from Event", Event.class ).getResultList();
+            for ( Event event : result ) {
+                System.out.println( "Event (" + event.getDate() + ") : " + event.getTitle() );
             }
         }
         // mvn exec:java -Dexec.mainClass="com.tyamap.simple_servlet.EventManager" -Dexec.args="emp"
+        // TODO: n+1問題
         else if (args[0].equals("emp")) {
-            List employees = mgr.listEmployee();
-            for (int i = 0; i < employees.size(); i++) {
-                Employee emp = (Employee) employees.get(i);
+            List<Employee> result = entityManager.createQuery( "from Employee", Employee.class ).getResultList();
+            for ( Employee emp : result ) {
                 System.out.println("Name: " + emp.getName() + " Events: " + emp.getEvents().size());
             }
         }
 
-        HibernateUtil.getSessionFactory().close();
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
-
-    private void createAndStoreEvent(String title, Date theDate) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-
-        Event theEvent = new Event();
-        theEvent.setTitle(title);
-        theEvent.setDate(theDate);
-        session.save(theEvent);
-
-        session.getTransaction().commit();
-    }
-
-    private List listEvents() {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Event").list();
-        session.getTransaction().commit();
-        return result;
-    }
-
-    private List listEmployee() {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Employee").list();
-        session.getTransaction().commit();
-        return result;
-    }
-
 }
