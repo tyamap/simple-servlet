25b7035f509e1ef89670a71e21ba6501d6941d15
tyamap <tom333my@outlook.com>
2020-09-05 17:43:21 +0900

    mod JPAで取得したリストを表示

--- src/main/java/com/tyamap/simple_servlet/TestHibernate.java
+++ src/main/java/com/tyamap/simple_servlet/TestHibernate.java
@@ -4,37 +4,40 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.Session;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.tyamap.simple_servlet.util.HibernateUtil;
+import com.tyamap.simple_servlet.domain.Event;
 
 @WebServlet("/test-hibernate.json")
 public class TestHibernate extends HttpServlet {
 
     // Exposeアノテーションがついていないものは除外する
     private Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+    private EntityManagerFactory entityManagerFactory =
+        Persistence.createEntityManagerFactory( "com.tyamap.simple_servlet.jpa" );
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
 
         PrintWriter out = response.getWriter();
-        EventManager mgr = new EventManager();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        try{
-            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-            session.beginTransaction();
-            List events = session.createQuery("from Event").list();
-            session.getTransaction().commit();
-            String jsonEvents = this.gson.toJson(events);
+        try {
+            entityManager.getTransaction().begin();
+            List<Event> result = entityManager.createQuery( "from Event", Event.class ).getResultList();
+            entityManager.getTransaction().commit();
+            entityManager.close();
+            String jsonEvents = this.gson.toJson(result);
 
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF-8");
