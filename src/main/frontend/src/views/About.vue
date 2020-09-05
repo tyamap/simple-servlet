e553b4191cd5b924cc3cf048ee82181713a03471
tyamap <tom333my@outlook.com>
2020-09-05 17:44:33 +0900

    rmv テスト用のファイルとページ

--- src/main/frontend/src/views/About.vue
+++ src/main/frontend/src/views/About.vue
@@ -3,11 +3,6 @@
     <h1>This is an about page</h1>
     <h2>To prove the app works, check following pages.</h2>
     <ul>
-      <li><a href="hello.jsp">JSP page</a></li>
-      <li><a href="test-servlet">Servlet</a></li>
-      <li><a href="test-db-connection">DB connection</a></li>
-      <hr>
-      <li><router-link to="/list">API communicate</router-link></li>
       <li><router-link to="/hibernate">Hibernate</router-link></li>
     </ul>
   </div>
