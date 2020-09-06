33da48f895d8736f035bc9550fc94ee376cdae7d
tyamap <tom333my@outlook.com>
2020-09-06 22:06:07 +0900

    add POST通信用の土台

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -3,8 +3,9 @@
     <h1>This is an about page</h1>
     <h2>To prove the app works, check following pages.</h2>
     <ul>
-      <li><router-link to="/events">Events list</router-link></li>
       <li><router-link to="/employees">Employees list</router-link></li>
+      <li><router-link to="/events">Events list</router-link></li>
+      <li><router-link to="/event/new">create an Event</router-link></li>
     </ul>
   </div>
 </template>
---
+++ frontend/src/views/Event/NewEvent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="f-event">
+    <h1>イベントの作成</h1>
+      <form id="form">
+        <!-- それぞれに作ったvaidatorに通す -->
+        <p>title:<input type="text" v-model="newEvent.title"  name='title' value=''></p>
+        <p>date:<input type="date" v-model="newEvent.date" name='date' value=''></p>
+        <p>host:<select name="host" v-model="newEvent.host">
+                  <option value="1">hoge</option>
+                  <option value="2">foo</option>
+                  <option value="3">bar</option>
+                </select>
+        </p>
+        <input type="submit" value="イベントを作成">
+      </form>
+      {{newEvent.title}}
+      {{newEvent.date}}
+      {{newEvent.host}}
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+// var BASE_URL = 'http://localhost:23450/webapp/'
+
+export default {
+  data () {
+    return {
+      newEvent: {
+        title: 'タイトル',
+        date: '2020-09-06',
+        host: 1,
+      }
+    }
+  },
+  actions: {
+    // axiosでPOST通信
+  }
+}
+</script>
