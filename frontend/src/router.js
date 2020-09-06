8bc3a99df9816c3a5f45be2de65c5dde73385fb5
tyamap <tom333my@outlook.com>
2020-09-06 20:24:12 +0900

    rmv TypeScript
    Vueの学習に集中したいため

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
 import Events from './views/Events.vue'
+import NewEvent from './views/Event/NewEvent.vue'
 import Employees from './views/Employees.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
@@ -18,10 +19,12 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
+    },
+    {
+      path: '/employees',
+      name: 'employees',
+      component: Employees
     },
     {
       path: '/events',
@@ -29,9 +32,9 @@ export default new Router({
       component: Events
     },
     {
-      path: '/employees',
-      name: 'employees',
-      component: Employees
+      path: '/event/new',
+      name: 'newEvent',
+      component: NewEvent
     }
   ]
 })
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,16 +6,14 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
 
-@Component({
+export default {
   components: {
     HelloWorld
   }
-})
-export default class Home extends Vue {}
+}
 </script>
 <style lang='scss' scoped>
 </style>
