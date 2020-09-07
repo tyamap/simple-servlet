69e0dbd9d4c8f5f6d451a010d846648ac6fe11a0
tyamap <tom333my@outlook.com>
2020-09-07 23:42:11 +0900

    add イベント追加のPOSTメソッド

--- frontend/src/views/Event/NewEvent.vue
+++ frontend/src/views/Event/NewEvent.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="f-event">
     <h1>イベントの作成</h1>
-      <form id="form">
-        <!-- それぞれに作ったvaidatorに通す -->
-        <p>title:<input type="text" v-model="newEvent.title"  name='title' value=''></p>
-        <p>date:<input type="date" v-model="newEvent.date" name='date' value=''></p>
-        <p>host:<select name="host" v-model="newEvent.host">
+      <form id="form" v-on:submit.prevent="postEvent('event/new', newEvent)">
+    　 <!-- TODO: バリデーション追加 -->
+        <p>title:<input type="text" v-model="newEvent.title"  name="title" value=""></p>
+        <p>date:<input type="date" v-model="newEvent.date" name="date" value=""></p>
+        <p>host:<select name="host" v-model="newEvent.host_id">
                   <option value="1">hoge</option>
                   <option value="2">foo</option>
                   <option value="3">bar</option>
@@ -20,21 +20,43 @@
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 
-// var BASE_URL = 'http://localhost:23450/webapp/'
+const BASE_URL = 'http://localhost:23450/webapp/'
 
 export default {
   data () {
     return {
       newEvent: {
         title: 'タイトル',
+        // TODO: 今日の日付（yyyy-MM-dd)
         date: '2020-09-06',
-        host: 1,
+        host_id: 1,
       }
     }
   },
-  actions: {
+  // TODO: actionsで書く
+  methods: {
+    postEvent(param, newEvent) {
+      return axios.post(BASE_URL + param, {
+        event: newEvent,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:23450'
+        }
+      })
+      .then(() => {
+        // レスポンスが200の時の処理
+        console.log('送れたよ')
+      })
+      .catch(err => {
+        if(err.response) {
+          // レスポンスが200以外の時の処理
+        console.log('NG')
+        }
+      });
+    }
     // axiosでPOST通信
   }
 }
